bb64a8797113ddb69acb5e0a4cff2b49a4023add
ivan-kuhta <filmplex22342@gmail.com>
2021-03-01 21:48:20 +0200

    feat: add function 'check'

--- src/index.js
+++ src/index.js
@@ -1,3 +1,10 @@
 module.exports = function check(str, bracketsConfig) {
-  // your solution
+    const regStr = bracketsConfig.map(item => item.join('').replace(/[^\w]/g, (letter) => `\\${letter}`)).join('|');
+    const reg = new RegExp(regStr, "g");
+    let tmpStr = str;
+
+    while(reg.test(tmpStr)){
+        tmpStr = tmpStr.replace(reg, "");
+    }
+    return !tmpStr.length;
 }
